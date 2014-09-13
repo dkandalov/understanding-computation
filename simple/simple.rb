f8acf8dfa62a7e8dc3104131e0270831913ffc1c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-13 16:15:16 +0100

    Variable

--- simple/simple.rb
+++ simple/simple.rb
@@ -1,6 +1,6 @@
-class Machine < Struct.new(:expression)
+class Machine < Struct.new(:expression, :environment)
   def step
-    self.expression = expression.reduce
+    self.expression = expression.reduce(environment)
   end
 
   def run
@@ -9,6 +9,24 @@ class Machine < Struct.new(:expression)
       step
     end
     puts expression
+  end
+end
+
+class Variable < Struct.new(:name)
+  def reduce(environment)
+    environment[name]
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
   end
 end
 
@@ -27,11 +45,11 @@ class Boolean < Struct.new(:value)
 end
 
 class LessThan < Struct.new(:left, :right)
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce, right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left, right.reduce)
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value < right.value)
     end
@@ -65,11 +83,11 @@ class Number < Struct.new(:value)
 end
 
 class Add < Struct.new(:left, :right)
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce, right)
+      Add.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce)
+      Add.new(left, right.reduce(environment))
     else
       Number.new(left.value + right.value)
     end
@@ -89,11 +107,11 @@ class Add < Struct.new(:left, :right)
 end
 
 class Multiply < Struct.new(:left, :right)
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new(left.reduce, right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new(left, right.reduce)
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
--- simple/simple_spec.rb
+++ simple/simple_spec.rb
@@ -12,6 +12,7 @@ describe 'Simple language' do
   end
 
   it 'reduce expressions in small steps' do
+    empty_environment = {}
     expression = Add.new(
       Multiply.new(Number.new(1), Number.new(2)),
       Multiply.new(Number.new(3), Number.new(4))
@@ -19,15 +20,15 @@ describe 'Simple language' do
     expect(expression.inspect).to eq('<<1 * 2 + 3 * 4>>')
 
     expect(expression.reducible?).to be(true)
-    expression = expression.reduce
+    expression = expression.reduce(empty_environment)
     expect(expression.inspect).to eq('<<2 + 3 * 4>>')
 
     expect(expression.reducible?).to be(true)
-    expression = expression.reduce
+    expression = expression.reduce(empty_environment)
     expect(expression.inspect).to eq('<<2 + 12>>')
 
     expect(expression.reducible?).to be(true)
-    expression = expression.reduce
+    expression = expression.reduce(empty_environment)
     expect(expression.inspect).to eq('<<14>>')
 
     expect(expression.reducible?).to be(false)
@@ -55,6 +56,19 @@ describe 'Simple language' do
         "5 < 2 + 2\n" +
         "5 < 4\n" +
         "false\n"
+    ).to_stdout
+  end
+
+  it 'expression with variable' do
+    machine = Machine.new(
+      Add.new(Variable.new(:x), Variable.new(:y)),
+      { x: Number.new(3), y: Number.new(4) }
+    )
+    expect{ machine.run }.to output(
+        "x + y\n" +
+        "3 + y\n" +
+        "3 + 4\n" +
+        "7\n"
     ).to_stdout
   end
 end
