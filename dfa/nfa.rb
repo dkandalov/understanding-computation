67d59f1c49bb93cc550fcb502afa0fec7af00e74
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-15 01:34:38 +0100

    nfa free moves

--- dfa/nfa.rb
+++ dfa/nfa.rb
@@ -6,6 +6,15 @@ class NFARulebook < Struct.new(:rules)
     states.flat_map{ |state| follow_rules_for(state, character) }.to_set
   end
 
+  def follow_free_moves(states)
+    more_states = next_states(states, nil)
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
+
   private
 
   def follow_rules_for(state, character)
@@ -28,5 +37,19 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
 
   def accepting?
     (current_states & accept_states).any?
+  end
+
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
+end
+
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap{ |nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    NFA.new(Set[start_state], accept_states, rulebook)
   end
 end
--- dfa/nfa_spec.rb
+++ dfa/nfa_spec.rb
@@ -2,6 +2,12 @@ require 'rspec'
 require File.expand_path('nfa.rb')
 
 describe 'NFA' do
+  it 'determine if in accepting state' do
+    some_rulebook = NFARulebook.new([])
+    expect(NFA.new(Set[1], [4], some_rulebook).accepting?).to be(false)
+    expect(NFA.new(Set[1, 2, 4], [4], some_rulebook).accepting?).to be(true)
+  end
+
   it 'should follow rules (see also nfa.png)' do
     rulebook = NFARulebook.new([
       FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
@@ -17,11 +23,23 @@ describe 'NFA' do
 
     nfa = NFA.new(Set[1], [4], rulebook)
     expect(nfa.read_string('bbb').accepting?).to be(true)
+
+    nfa_design = NFADesign.new(1, [4], rulebook)
+    expect(nfa_design.accepts?('bab')).to be(true)
+    expect(nfa_design.accepts?('bbbbb')).to be(true)
+    expect(nfa_design.accepts?('bbabb')).to be(false)
   end
 
-  it 'determine if in accepting state' do
-    some_rulebook = NFARulebook.new([])
-    expect(NFA.new([1], [4], some_rulebook).accepting?).to be(false)
-    expect(NFA.new([1, 2, 4], [4], some_rulebook).accepting?).to be(true)
+  it 'should follow rules with free moves' do
+    rulebook = NFARulebook.new([
+      FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+      FARule.new(2, 'a', 3), FARule.new(3, 'a', 2),
+      FARule.new(4, 'a', 5), FARule.new(5, 'a', 6),
+      FARule.new(6, 'a', 4)
+    ])
+    nfa_design = NFADesign.new(1, [2, 4], rulebook)
+    expect(nfa_design.accepts?('aa')).to be(true)
+    expect(nfa_design.accepts?('aaa')).to be(true)
+    expect(nfa_design.accepts?('aaaaa')).to be(false)
   end
 end
