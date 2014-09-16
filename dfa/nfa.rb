082e76fd815a6d0920c68f29d03769d4a61cdd9c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-16 22:37:50 +0100

    nfa to dfa conversion

--- dfa/nfa.rb
+++ dfa/nfa.rb
@@ -3,7 +3,7 @@ require_relative './dfa.rb'
 
 class NFARulebook < Struct.new(:rules)
   def next_states(states, character)
-    states.flat_map{ |state| follow_rules_for(state, character) }.to_set
+    states.flat_map { |state| follow_rules_for(state, character) }.to_set
   end
 
   def follow_free_moves(states)
@@ -15,18 +15,22 @@ class NFARulebook < Struct.new(:rules)
     end
   end
 
+  def alphabet
+    rules.map(&:character).compact.uniq
+  end
+
   private
 
   def follow_rules_for(state, character)
     rules
-      .select{ |rule| rule.applies_to?(state, character) }
-      .map(&:follow)
+        .select { |rule| rule.applies_to?(state, character) }
+        .map(&:follow)
   end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
   def read_string(string)
-    string.chars.each{ |char| read_character(char) }
+    string.chars.each { |char| read_character(char) }
     self
   end
 
@@ -46,10 +50,44 @@ end
 
 class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
   def accepts?(string)
-    to_nfa.tap{ |nfa| nfa.read_string(string) }.accepting?
+    to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
   end
 
-  def to_nfa
-    NFA.new(Set[start_state], accept_states, rulebook)
+  def to_nfa(current_states = Set[start_state])
+    NFA.new(current_states, accept_states, rulebook)
   end
 end
+
+class NFASimulation < Struct.new(:nfa_design)
+  def to_dfa_design
+    start_state = nfa_design.to_nfa.current_states
+    states, rules = discover_states_and_rules(Set[start_state])
+    accept_states = states.select{ |state| nfa_design.to_nfa(state).accepting? }
+
+    DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
+  end
+
+  def discover_states_and_rules(states)
+    rules = states.flat_map { |state| rules_for(state) }.uniq
+    more_states = rules.map(&:follow).to_set
+
+    if more_states.subset?(states)
+      [states, rules]
+    else
+      discover_states_and_rules(states + more_states)
+    end
+  end
+
+  def rules_for(state)
+    nfa_design.rulebook.alphabet.map do |character|
+      FARule.new(state, character, next_state(state, character))
+    end
+  end
+
+  def next_state(state, character)
+    nfa_design.to_nfa(state).tap { |nfa|
+      nfa.read_character(character)
+    }.current_states
+  end
+end
+
--- dfa/nfa_spec.rb
+++ dfa/nfa_spec.rb
@@ -42,4 +42,60 @@ describe 'NFA' do
     expect(nfa_design.accepts?('aaa')).to be(true)
     expect(nfa_design.accepts?('aaaaa')).to be(false)
   end
+
 end
+
+describe 'NFA to DFA conversion' do
+  it 'next steps can be simulated' do
+    rulebook = NFARulebook.new([
+       FARule.new(1, 'a', 1), FARule.new(1, 'a', 2), FARule.new(1, nil, 2),
+       FARule.new(2, 'b', 3),
+       FARule.new(3, 'b', 1), FARule.new(3, nil, 2)
+    ])
+    nfa_design = NFADesign.new(1, [3], rulebook)
+    simulation = NFASimulation.new(nfa_design)
+
+    expect(simulation.next_state(Set[1, 2], 'a')).to eq(Set[1, 2])
+    expect(simulation.next_state(Set[1, 2], 'b')).to eq(Set[2, 3])
+    expect(simulation.next_state(Set[3, 2], 'b')).to eq(Set[1, 3, 2])
+    expect(simulation.next_state(Set[1, 3, 2], 'b')).to eq(Set[1, 3, 2])
+    expect(simulation.next_state(Set[1, 3, 2], 'a')).to eq(Set[1, 2])
+  end
+
+  it 'rules can be found for any state' do
+    rulebook = NFARulebook.new([
+       FARule.new(1, 'a', 1), FARule.new(1, 'a', 2), FARule.new(1, nil, 2),
+       FARule.new(2, 'b', 3),
+       FARule.new(3, 'b', 1), FARule.new(3, nil, 2)
+    ])
+    nfa_design = NFADesign.new(1, [3], rulebook)
+    simulation = NFASimulation.new(nfa_design)
+
+    expect(rulebook.alphabet).to eq(['a', 'b'])
+    expect(simulation.rules_for(Set[1, 2])).to eq([
+      FARule.new(Set[1, 2], 'a', Set[1, 2]),
+      FARule.new(Set[1, 2], 'b', Set[3, 2]),
+    ])
+    expect(simulation.rules_for(Set[3, 2])).to eq([
+      FARule.new(Set[3, 2], 'a', Set[]),
+      FARule.new(Set[3, 2], 'b', Set[1, 3, 2]),
+    ])
+  end
+
+  it 'output DFA works same way as NFA' do
+    rulebook = NFARulebook.new([
+       FARule.new(1, 'a', 1), FARule.new(1, 'a', 2), FARule.new(1, nil, 2),
+       FARule.new(2, 'b', 3),
+       FARule.new(3, 'b', 1), FARule.new(3, nil, 2)
+    ])
+    nfa_design = NFADesign.new(1, [3], rulebook)
+    simulation = NFASimulation.new(nfa_design)
+
+    dfa_design = simulation.to_dfa_design
+
+    expect(dfa_design.accepts?('aaa')).to be(false)
+    expect(dfa_design.accepts?('aab')).to be(true)
+    expect(dfa_design.accepts?('bbbabb')).to be(true)
+  end
+end
+
