4c1ae442f3a08f849c86ae76c7507adeba0bd83a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-19 18:24:32 +0100

    dpda palindrome example

--- pda/dpda_spec.rb
+++ pda/dpda_spec.rb
@@ -117,5 +117,29 @@ describe 'DPDA design' do
     expect(dpda_design.accepts?('bbbaaaab')).to be(true)
     expect(dpda_design.accepts?('baa')).to be(false)
   end
+
+  it 'can recognize palindrome made from a and b' do
+    rules = [
+        PDARule.new(1, 'a', 1, '$', %w(a $)),
+        PDARule.new(1, 'a', 1, 'a', %w(a a)),
+        PDARule.new(1, 'a', 1, 'b', %w(a b)),
+        PDARule.new(1, 'b', 1, '$', %w(b $)),
+        PDARule.new(1, 'b', 1, 'a', %w(b a)),
+        PDARule.new(1, 'b', 1, 'b', %w(b b)),
+        PDARule.new(1, 'm', 2, '$', ['$']),
+        PDARule.new(1, 'm', 2, 'a', ['a']),
+        PDARule.new(1, 'm', 2, 'b', ['b']),
+        PDARule.new(2, 'a', 2, 'a', []),
+        PDARule.new(2, 'b', 2, 'b', []),
+        PDARule.new(2, nil, 3, '$', ['$'])
+    ]
+    dpda_design = DPDADesign.new(1, '$', [3], DPDARulebook.new(rules))
+
+    expect(dpda_design.accepts?('ama')).to be(true)
+    expect(dpda_design.accepts?('abmba')).to be(true)
+    expect(dpda_design.accepts?('babbamabbab')).to be(true)
+    expect(dpda_design.accepts?('abmb')).to be(false)
+    expect(dpda_design.accepts?('baambaa')).to be(false)
+  end
 end
 
