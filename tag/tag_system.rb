9a6693e942a5f29188fc7927f56b92d834441702
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-26 19:05:46 +0100

    cyclic tag system

--- tag/tag_system.rb
+++ tag/tag_system.rb
@@ -6,6 +6,14 @@ class TagRule < Struct.new(:first_character, :append_character)
   def follow(string)
     string + append_character
   end
+
+  def alphabet
+    ([first_character] + append_character.chars).uniq
+  end
+
+  def to_cyclic(encoder)
+    CyclicTagRule.new(encoder.encode_string(append_character))
+  end
 end
 
 class TagRuleBook < Struct.new(:deletion_number, :rules)
@@ -20,12 +28,37 @@ class TagRuleBook < Struct.new(:deletion_number, :rules)
   def rule_for(string)
     rules.detect { |r| r.applies_to?(string) }
   end
+
+  def to_cyclic(encoder)
+    CyclicTagRuleBook.new(cyclic_rules(encoder) + cyclic_padding_rules(encoder))
+  end
+
+  def alphabet
+    rules.flat_map(&:alphabet).uniq
+  end
+
+  def cyclic_rules(encoder)
+    encoder.alphabet.map { |character| cyclic_rule_for(character, encoder) }
+  end
+
+  def cyclic_rule_for(character, encoder)
+    rule = rule_for(character)
+    if rule.nil?
+      CyclicTagRule.new('')
+    else
+      rule.to_cyclic(encoder)
+    end
+  end
+
+  def cyclic_padding_rules(encoder)
+    Array.new(encoder.alphabet.length, CyclicTagRule.new('')) * (deletion_number - 1)
+  end
 end
 
 class TagSystem < Struct.new(:current_string, :rulebook)
-  def run
+  def run(step_limit = 2**32)
     steps = []
-    while rulebook.applies_to?(current_string)
+    while rulebook.applies_to?(current_string) and steps.size < step_limit
       steps.push(current_string)
       step
     end
@@ -35,5 +68,66 @@ class TagSystem < Struct.new(:current_string, :rulebook)
 
   def step
     self.current_string = rulebook.next_string(current_string)
+  end
+
+  def encoder
+    CyclicTagRuleTagEncoder.new(alphabet)
+  end
+
+  def alphabet
+    (rulebook.alphabet + current_string.chars).uniq.sort
+  end
+
+  def to_cyclic
+    TagSystem.new(encoder.encode_string(current_string), rulebook.to_cyclic(encoder))
+  end
+end
+
+class CyclicTagRuleTagEncoder < Struct.new(:alphabet)
+  def encode_string(string)
+    string.chars.map { |character| encode_character(character) }.join
+  end
+
+  def encode_character(character)
+    character_position = alphabet.index(character)
+    (0...alphabet.length).map{ |n| n == character_position ? '1' : '0' }.join
+  end
+end
+
+class CyclicTagRule < TagRule
+  FIRST_CHARACTER = '1' # this is a part of cyclic tag system definition
+
+  def initialize(append_characters)
+    super(FIRST_CHARACTER, append_characters)
+  end
+
+  def inspect
+    "#<CyclicTagRule #{append_character.inspect}>"
+  end
+end
+
+class CyclicTagRuleBook < Struct.new(:rules)
+  DELETION_NUMBER = 1 # this is a part of cyclic tag system definition
+
+  def initialize(rules)
+    # Array.cycle creates infinite stream
+    super(rules.cycle)
+  end
+
+  def applies_to?(string)
+    string.length >= DELETION_NUMBER
+  end
+
+  def next_string(string)
+    follow_next_rule(string).slice(DELETION_NUMBER..-1)
+  end
+
+  def follow_next_rule(string)
+    rule = rules.next
+    if rule.applies_to?(string)
+      rule.follow(string)
+    else
+      string
+    end
   end
 end
--- tag/tag_system_spec.rb
+++ tag/tag_system_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'rspec/core/example_group'
 require_relative 'tag_system'
 
 describe 'Tag system' do
@@ -17,19 +18,15 @@ describe 'Tag system' do
     six = 'aabbbbbbbbbbbb'
     system = TagSystem.new(six, rulebook)
 
-    three = "ccdddddd"
+    three = 'ccdddddd'
     expect(system.run).to eq(
       ['aabbbbbbbbbbbb', 'bbbbbbbbbbbbcc', 'bbbbbbbbbbccd', 'bbbbbbbbccdd',
        'bbbbbbccddd', 'bbbbccdddd', 'bbccddddd', three])
   end
 
   it 'can increment number' do
-    rulebook = TagRuleBook.new(2, [TagRule.new('a' ,'ccdd'), TagRule.new('b', 'dd')])
-    two = 'aabbbb'
-    system = TagSystem.new(two, rulebook)
-
-    three = "ccdddddd"
-    expect(system.run).to eq(['aabbbb', 'bbbbccdd', 'bbccdddd', three])
+    three = 'ccdddddd'
+    expect(inc_system.run).to eq(['aabbbb', 'bbbbccdd', 'bbccdddd', three])
   end
 
   it 'can combine rules' do
@@ -58,5 +55,43 @@ describe 'Tag system' do
     five = 'aabbbbbbbbbb'
     system = TagSystem.new(five, rulebook)
     expect(system.run.last).to eq('o')
+  end
+
+  it 'has alphabet' do
+    rulebook = TagRuleBook.new(2, [TagRule.new('a' ,'ccdd'), TagRule.new('b', 'dd')])
+    system = TagSystem.new('aabbbb', rulebook)
+
+    expect(system.alphabet).to eq(%w(a b c d))
+  end
+
+  it 'can encode characters for cyclic tag system' do
+    expect(inc_system.encoder.encode_string('cab')).to eq('001010000100')
+  end
+
+  it 'can be converted to cyclic tag system' do
+    cyclic_system = inc_system.to_cyclic
+    expect(cyclic_system.run.last(5)).to eq(['0001', '001', '01', '1', ''])
+  end
+
+  let(:inc_system) do
+    rulebook = TagRuleBook.new(2, [TagRule.new('a' ,'ccdd'), TagRule.new('b', 'dd')])
+    two = 'aabbbb'
+    TagSystem.new(two, rulebook)
+  end
+end
+
+describe 'Cyclic tag system' do
+  it 'can stabilize to a repetitive behavior' do
+    rules = [CyclicTagRule.new('1'), CyclicTagRule.new('0010'), CyclicTagRule.new('10')]
+    rulebook = CyclicTagRuleBook.new(rules)
+    system = TagSystem.new('11', rulebook)
+
+    pattern = %w(0101 101 011 11 110 101 010010 10010 00101)
+    expect(system.run(43)).to eq([
+       '11', '11', '10010', '001010', '01010', '1010',
+       '01010', '1010', '0100010', '100010', '000101',
+       '00101', '0101', '101', '010010', '10010', '00101',
+       pattern, pattern, pattern
+    ].flatten)
   end
 end
